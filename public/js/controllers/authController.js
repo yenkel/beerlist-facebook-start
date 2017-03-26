42131a68ee0b9783b7c238d71f04ecbc21fb66dc
Steven Winston <swinston100@hotmail.com>
2017-03-27 01:31:52 +0300

    Update authController.js

--- public/js/controllers/authController.js
+++ public/js/controllers/authController.js
@@ -1,22 +1,7 @@
 app.controller('AuthCtrl', function($scope, authFactory, $state) {
-
-  $scope.register = function() {
-    authFactory.register($scope.user)
-      .then(function() {
-        $state.go('home');
-      }, function(err) {
-        alert(err.data.message);
-      });
-  }
-
   $scope.currentUser = authFactory.currentUser
 
   $scope.login = function() {
-    authFactory.login($scope.user)
-      .then(function() {
-        $state.go('home');
-      }, function(err) {
-        alert(err.data);
-      });
+    //todo
   }
 });
