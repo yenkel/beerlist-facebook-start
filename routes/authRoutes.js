c916883a6527caee93f5fdda33b729904f8a0ed0
Steven Winston <swinston100@hotmail.com>
2017-03-27 00:58:10 +0300

    remove code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,13 +2,4 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-router.get('/auth/facebook', passport.authenticate('facebook'));
-
-router.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { session: false }),
-  function(req, res) {
-    res.send(req.user);
-  }
-);
-
 module.exports = router;
