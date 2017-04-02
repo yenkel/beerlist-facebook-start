39a884da9e8e0d4f98349ab64d7c4c44cfe2f82b
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-04-02 14:13:39 +0300

    new routes

--- models/passport.js
+++ models/passport.js
@@ -1,5 +1,23 @@
+var FacebookStrategy = require('passport-facebook').Strategy;
 var passport = require('passport');
 
 //passport configuration here
+passport.use(new FacebookStrategy({
+        clientID: 1357380317683447,
+        clientSecret: "e8cff837dca361751fe84d04635a5a9f",
+        callbackURL: 'http://localhost:8000/auth/facebook/callback',
+        profileFields: ['email', 'displayName']
+
+    },
+    function(accessToken, refreshToken, profile, done) {
+
+        //code to check database goes here
+
+        //code to create JWT goes here
+
+        return done(null, profile)
+    }
+));
+
 
 module.exports = passport;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,4 +2,15 @@ var express = require('express');
 var router = express.Router();
 var passport = require('../models/passport');
 
+
+router.get('/facebook', passport.authenticate('facebook', { scope: 'email' }));
+
+router.get('/facebook/callback',
+    passport.authenticate('facebook', { session: false, failureRedirect: "/" }),
+    function(req, res) {
+        console.log(req.user);
+        res.redirect('/');
+    }
+);
+
 module.exports = router;
